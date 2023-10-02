d2ed5eaf68b836ac009d37f7aee0c2a8bc2d1846
martinooliveri <martinooliveri@gmail.com>
2023-10-02 10:40:24 -0300

    prueba

--- js/codigo.js
+++ js/codigo.js
@@ -4,4 +4,5 @@ function cargarTransaccion()
 {
     let transaccionValor = Number(document.querySelector("#txtValorCompraIngresada").value);
     let transaccionEnDolares = document.querySelector("#checkCompraEnDolares").checked;
+    let transaccionTipoCambio = Number(document.querySelector("#txtCotizacionDolarIngresada").value);
 }
